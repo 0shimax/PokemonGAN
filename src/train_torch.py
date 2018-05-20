8dd677f89f60497422b2b7465dd42b710bcb42c8
0shimax <shin.keeper@gmail.com>
2018-05-20 18:54:23 +0900

    modify

--- src/train_torch.py
+++ src/train_torch.py
@@ -17,7 +17,7 @@ import torchvision.utils as vutils
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', default='folder', help='cifar10 | lsun | imagenet | folder | lfw | fake')
 parser.add_argument('--dataroot', default='./data', help='path to dataset')
-parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
 parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
@@ -100,10 +100,10 @@ class Generator(nn.Module):
         self.s4 = 4
 
         self.g_fc1 = nn.Linear(100, self.s4*self.s4*self.c4)
-        self.g_h1 = nn.ConvTranspose2d(self.c4, self.c8, 5, 2, padding=2, bias=False)
-        self.g_h2 = nn.ConvTranspose2d(self.c8, self.c16, 5, 2, padding=1, bias=False)
-        self.g_h3 = nn.ConvTranspose2d(self.c16, self.c32, 5, 2, padding=1, bias=False)
-        self.g_h4 = nn.ConvTranspose2d(self.c32, 3, 4, 2, padding=0, bias=False)
+        self.g_h1 = nn.ConvTranspose2d(self.c4, self.c8, 5, 2, padding=2)
+        self.g_h2 = nn.ConvTranspose2d(self.c8, self.c16, 5, 2, padding=1)
+        self.g_h3 = nn.ConvTranspose2d(self.c16, self.c32, 5, 2, padding=1)
+        self.g_h4 = nn.ConvTranspose2d(self.c32, 3, 4, 2, padding=0)
 
         self.bn0 = nn.BatchNorm2d(self.c4)
         self.bn1 = nn.BatchNorm2d(self.c8)
@@ -114,16 +114,16 @@ class Generator(nn.Module):
     def forward(self, input):
         h_z = self.g_fc1(input)
         h0 = h_z.view(-1, self.c4, self.s4, self.s4)
-        h0 = F.leaky_relu(self.bn0(h0))
+        h0 = F.relu(self.bn0(h0))
 
         h1 = self.g_h1(h0)
-        h1 = F.leaky_relu(self.bn1(h1))
+        h1 = F.relu(self.bn1(h1))
 
         h2 = self.g_h2(h1)
-        h2 = F.leaky_relu(self.bn2(h2))
+        h2 = F.relu(self.bn2(h2))
 
         h3 = self.g_h3(h2)
-        h3 = F.leaky_relu(self.bn3(h3))
+        h3 = F.relu(self.bn3(h3))
 
         h4 = self.g_h4(h3)
 
@@ -146,10 +146,10 @@ class Discriminator(nn.Module):
         super().__init__()
         self.c2, self.c4, self.c8, self.c16 = 64, 128, 256, 512
 
-        self.d_h0_conv = nn.Conv2d(3, self.c2, 5, 2, padding=0, bias=False)
-        self.d_h1_conv = nn.Conv2d(self.c2, self.c4, 5, 2, padding=0, bias=False)
-        self.d_h2_conv = nn.Conv2d(self.c4, self.c8, 5, 2, padding=0, bias=False)
-        self.d_h3_conv = nn.Conv2d(self.c8, self.c16, 5, 2, padding=0, bias=False)
+        self.d_h0_conv = nn.Conv2d(3, self.c2, 5, 2, padding=0)
+        self.d_h1_conv = nn.Conv2d(self.c2, self.c4, 5, 2, padding=0)
+        self.d_h2_conv = nn.Conv2d(self.c4, self.c8, 5, 2, padding=0)
+        self.d_h3_conv = nn.Conv2d(self.c8, self.c16, 5, 2, padding=0)
         self.d_fc = nn.Linear(self.c16, 1)
 
         self.bn0 = nn.BatchNorm2d(self.c2)
@@ -173,8 +173,8 @@ class Discriminator(nn.Module):
         h3 = F.leaky_relu(self.bn3(h3))
 
         h3_flatten = h3.view(n_batch, -1)
-        out = F.sigmoid(self.d_fc(h3_flatten).squeeze(1))
-        return out
+        # out = F.sigmoid(self.d_fc(h3_flatten).squeeze(1))
+        return self.d_fc(h3_flatten).squeeze(1)
 
 
 netD = Discriminator()
@@ -188,7 +188,8 @@ if opt.netD != '':
 
 
 def main():
-    fixed_noise = torch.randn(opt.batchSize, nz)
+    # fixed_noise = torch.randn(opt.batchSize, nz)  # .clamp_(-1., 1.).detach()
+    fixed_noise = torch.FloatTensor(opt.batchSize, nz).uniform_(-1, 1)
 
     # setup optimizer
     # optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
@@ -206,9 +207,14 @@ def main():
             optimizerD.zero_grad()
 
             # train with fake
-            noise = torch.randn(opt.batchSize, nz)
+            # noise = torch.randn(opt.batchSize, nz)  # .clamp_(-1., 1.).detach()
+            noise = torch.FloatTensor(opt.batchSize, nz).uniform_(-1, 1)
             fake = netG(noise).detach()
             # Adversarial loss
+            # err1 = -torch.mean(netD(real_images))
+            # err2 = torch.mean(netD(fake))
+            # loss_D = err1 + err2
+            # print(err2.item(), err1.item())
             loss_D = -torch.mean(netD(real_images)) + torch.mean(netD(fake))
             loss_D.backward()
             optimizerD.step()
@@ -221,7 +227,7 @@ def main():
                 # (2) Update G network: maximize log(D(G(z)))
                 ###########################
                 optimizerG.zero_grad()
-                fake = netG(noise)
+                fake = netG(fixed_noise)
                 # Adversarial loss
                 loss_G = -torch.mean(netD(fake))
                 loss_G.backward()
